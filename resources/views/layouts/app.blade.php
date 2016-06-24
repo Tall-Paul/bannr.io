2c155180fb255300c70e68759ba958670700788b
Paul Berry <paul.berry@prettylittlething.com>
2016-06-24 12:12:28 +0100

    missed some assets from last commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,9 +14,9 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
-    <link href="{{{ asset('/css/editor.css') }}}" rel="stylesheet">
-    <link href="{{{ asset('/css/spectrum.css') }}}" rel="stylesheet">
-    <link href="{{{ asset('/css/datetimepicker.css') }}}" rel="stylesheet">
+    <link href="{{{ secure_asset('/css/editor.css') }}}" rel="stylesheet">
+    <link href="{{{ secure_asset('/css/spectrum.css') }}}" rel="stylesheet">
+    <link href="{{{ secure_asset('/css/datetimepicker.css') }}}" rel="stylesheet">
     <style>
         body {
             font-family: 'Lato';
