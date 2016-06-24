4e33cf198a92422d0c4773a44de29deeba8c9b31
Paul Berry <paul.berry@prettylittlething.com>
2016-06-24 16:42:25 +0100

    Actual fix for country code

--- resources/views/loader.blade.php
+++ resources/views/loader.blade.php
@@ -4,6 +4,6 @@
 
 var bannr_data = JSON.parse(<?php echo(json_encode(html_entity_decode($data))); ?>);
 var script = document.createElement('script');
-script.setAttribute('src', 'http://{{$site}}/js/bQuery.js');
+script.setAttribute('src', 'http://loader.bannr.io/js/bQuery.js');
 script.setAttribute('type', 'text/javascript');
 document.getElementsByTagName('head')[0].appendChild(script);
