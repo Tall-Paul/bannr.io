e4942de370a74a061ca5e749064a38bdfb0990cd
Paul Berry <paul.berry@prettylittlething.com>
2016-06-26 20:22:10 +0100

    add https to routes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        if (App::environment('production')) {
+           URL::forceSchema('https');
+        }
     }
 
     /**
