8b51c95e9653969a5f5c04bc5f8a111603902420
Paul Berry <paul.berry@prettylittlething.com>
2016-07-03 16:20:21 +0100

    fix for proxy route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Providers;
+
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
@@ -26,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        App::removeMiddleware('Illuminate\Http\FrameGuard');
+        App::forgetMiddleware('Illuminate\Http\FrameGuard');
     }
 }
