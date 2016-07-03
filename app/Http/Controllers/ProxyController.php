dec8497ff4661c344b44287586b22666f39b665e
Paul Berry <paul.berry@prettylittlething.com>
2016-07-03 17:06:05 +0100

    inject styles and js

--- app/Http/Controllers/ProxyController.php
+++ app/Http/Controllers/ProxyController.php
@@ -41,6 +41,7 @@ class ProxyController extends Controller
          <link rel='stylesheet' href='https://admin.bannr.io/css/inject.css'></link>
          </head>
         ",$response);
+        $response = str_replace("=\"\\","=\"".$proxy_url."\\",$response);
         echo $response;
     }
 
