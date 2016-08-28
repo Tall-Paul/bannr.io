ee76a489b88fe3ffdbd0375cdfebc2b6b367a8c0
Paul Berry <paul.berry@prettylittlething.com>
2016-08-28 16:42:19 +0100

    redirect to home on login

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -28,7 +28,7 @@ class AuthController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'teams';
+    protected $redirectTo = 'home';
 
     /**
      * Create a new authentication controller instance.
