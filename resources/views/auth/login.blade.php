12ac7722d9c38fd8e1b3e2467da3df58927f3ff6
Paul@berry-briggs.com <Paul@berry-briggs.com>
2018-12-04 11:08:20 +0000

    remove demo data

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,10 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
+                <div class="panel-body">
+                    <div class="row">
+        
+                </div>
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
 
